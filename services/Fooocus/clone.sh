1382e542a11557a690e30df00360ae3a0c982ce6
web3nomad.eth <me@web3nomad.eth>
2023-10-07 17:56:27 +0800

    upgrade fooocus to 2.0.85

--- services/Fooocus/clone.sh
+++ services/Fooocus/clone.sh
@@ -8,4 +8,6 @@ git init
 git remote add origin "$2"
 git fetch origin "$3" --depth=1
 git reset --hard "$3"
-rm -rf .git
+
+# rm -rf .git
+# DO NOT REMOVE /.git, see https://github.com/lllyasviel/Fooocus/blob/b42e96a52d92265fb966954090aa43340f199008/modules/launch_util.py#L41
