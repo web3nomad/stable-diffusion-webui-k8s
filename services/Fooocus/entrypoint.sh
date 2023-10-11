ad883247be3a6001e1b6a02cc71ea6b4145b3946
web3nomad.eth <me@web3nomad.eth>
2023-10-11 11:29:40 +0800

    upgrade to fooocus2.0, comfy clone is not required

--- services/Fooocus/entrypoint.sh
+++ services/Fooocus/entrypoint.sh
@@ -2,27 +2,15 @@
 
 set -Eeuo pipefail
 
-mkdir -p /data/models/checkpoints
-mkdir -p /data/models/loras
-mkdir -p /data/models/inpaint
-mkdir -p /data/models/vae_approx
-mkdir -p /data/models/upscale_models
-mkdir -p /data/models/prompt_expansion
+for i in checkpoints loras inpaint vae_approx upscale_models prompt_expansion controlnet; do
+  mkdir -p /data/models/$i
+done
 
 rsync -a --info=NAME /Fooocus/models/prompt_expansion/ /data/models/prompt_expansion/
 
-rm -rf /Fooocus/models/checkpoints
-rm -rf /Fooocus/models/loras
-rm -rf /Fooocus/models/inpaint
-rm -rf /Fooocus/models/vae_approx
-rm -rf /Fooocus/models/upscale_models
-rm -rf /Fooocus/models/prompt_expansion
-
-ln -s /data/models/checkpoints /Fooocus/models/checkpoints
-ln -s /data/models/loras /Fooocus/models/loras
-ln -s /data/models/inpaint /Fooocus/models/inpaint
-ln -s /data/models/vae_approx /Fooocus/models/vae_approx
-ln -s /data/models/upscale_models /Fooocus/models/upscale_models
-ln -s /data/models/prompt_expansion /Fooocus/models/prompt_expansion
+for i in checkpoints loras inpaint vae_approx upscale_models prompt_expansion controlnet; do
+  rm -rf /Fooocus/models/$i
+  ln -s /data/models/$i /Fooocus/models/$i
+done
 
 exec "$@"
