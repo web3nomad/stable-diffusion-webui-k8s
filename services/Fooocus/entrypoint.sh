732c287c111250fd862e1a24bf19603325d2c40b
zhuojg <zhuojg1519@outlook.com>
2023-10-12 19:29:02 +0800

    copy clip_vision in entrypoint

--- services/Fooocus/entrypoint.sh
+++ services/Fooocus/entrypoint.sh
@@ -8,7 +8,7 @@ done
 
 rsync -a --info=NAME /Fooocus/models/prompt_expansion/ /data/models/prompt_expansion/
 
-for i in checkpoints loras inpaint vae_approx upscale_models prompt_expansion controlnet; do
+for i in checkpoints loras inpaint vae_approx upscale_models prompt_expansion controlnet clip_vision; do
   rm -rf /Fooocus/models/$i
   ln -s /data/models/$i /Fooocus/models/$i
 done
