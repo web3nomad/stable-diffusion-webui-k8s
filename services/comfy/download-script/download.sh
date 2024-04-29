20ac9fa07895eed1ceca30cd5750035b19929beb
web3nomad.eth <me@web3nomad.eth>
2024-04-29 23:13:29 +0800

    add ComfyUI_Invisible_Watermark

--- services/comfy/download-script/download.sh
+++ services/comfy/download-script/download.sh
@@ -18,6 +18,7 @@ echo "Clone the repositories of custom nodes..."
 ./clone.sh comfyui-tooling-nodes https://github.com/Acly/comfyui-tooling-nodes.git 7fc46f18f9f9751865588a9c74d0e058ba7afd90
 ./clone.sh efficiency-nodes-comfyui https://github.com/jags111/efficiency-nodes-comfyui.git b0ab993a33f5bec86b13806dcc1a9b41b76cb174
 ./clone.sh ComfyUI_Base64_Image https://github.com/web3nomad/ComfyUI_Base64_Image.git 1bf9bc2237e800f8c2058310d6dc8bca1d92b40c
+./clone.sh ComfyUI_Invisible_Watermark git@github.com:web3nomad/ComfyUI_Invisible_Watermark.git f5eeaff9e36d1cc51f1d21a0e881316536e26466
 
 echo "Downloading, this might take a while..."
 
