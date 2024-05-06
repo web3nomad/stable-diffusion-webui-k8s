d4bd0237ddb71947e121963aa172c6ed8d23bb3a
web3nomad.eth <me@web3nomad.eth>
2024-05-06 12:19:30 +0800

    set instance name for different purpose

--- ppio/scripts/createInstance.mjs
+++ ppio/scripts/createInstance.mjs
@@ -3,17 +3,32 @@ import { post } from './request.mjs'
 const imageAuth = process.env.IMAGE_AUTH;
 
 const instances = {
-  "3090": {
-    name: "RTX 3090 24GB",
+  "dev": {
+    name: "机器学习平台",
+    productId: "23",
+    clusterId: "22",
+    networkStorageId: "fs-2SEgwSx7coKjPKE2C8Squ7",
+    imageAuth: "",
+    imageUrl: "image.paigpu.com/prod-gpucloudpublic/cuda:12.2.2-devel-ubuntu22.04",
+    ports: "22,3000,4000,8000,8080,8888,7860",
+  },
+  "yum-3090": {
+    name: "YUM ComfyUI",
     productId: "23",
     clusterId: "22",
     networkStorageId: "fs-B7x768uRvLYJUW4Ev6ijpM",
+    imageAuth: imageAuth,
+    imageUrl: "image.paigpu.com/prod-htrwqusyqkkyvzawouyy/comfyui:cdb636f",
+    ports: "7860",
   },
-  "4090": {
-    name: "RTX 4090 24GB",
+  "yum-4090": {
+    name: "YUM ComfyUI",
     productId: "75",
     clusterId: "26",
     networkStorageId: "fs-oU4n7uWn5D6F4ZtXnaiXPD",
+    imageAuth: imageAuth,
+    imageUrl: "image.paigpu.com/prod-htrwqusyqkkyvzawouyy/comfyui:cdb636f",
+    ports: "7860",
   },
 }
 
@@ -23,9 +38,6 @@ function main() {
     gpuNum: 1,
     diskSize: 100,
     billingMode: "afterusage",
-    imageUrl: "image.paigpu.com/prod-htrwqusyqkkyvzawouyy/comfyui:cdb636f",
-    imageAuth: imageAuth,
-    ports: "7860",
     envs: [],
     networkStorageMountPoint: "/data",
     ...instances[gpuName],
--- ppio/scripts/listInstances.mjs
+++ ppio/scripts/listInstances.mjs
@@ -1,13 +1,16 @@
 import { get } from './request.mjs'
 
 function main() {
+  const name = process.argv[2];
   get('/v1/gpu/instances').then(data => {
     const items = data.instances
       .filter(item => item.status === 'running')
       .filter(item => item.imageName === 'image.paigpu.com/prod-htrwqusyqkkyvzawouyy/comfyui:cdb636f')
+      .filter(item => item.name.includes(name))
       .map((item) => {
-        const { sshCommand, rootPassword, portMappings } = item;
+        const { name, sshCommand, rootPassword, portMappings } = item;
         return {
+          name,
           sshCommand,
           rootPassword,
           portMapping: portMappings[0],
